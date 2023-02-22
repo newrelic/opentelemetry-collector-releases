ad3e63280b05977711f20e260b9ffe4fae0357fd
Grigorii Merkushev <brushknight@gmail.com>
2023-02-22 17:26:25 +0100

    trigger pipeline

--- ci/ci.mk
+++ ci/ci.mk
@@ -5,20 +5,20 @@ terraform/backend:
 ifndef TERRAFORM_BACKEND
 	$(error TERRAFORM_BACKEND is undefined)
 endif
-	sed "s/REPLACE_ME/${TERRAFORM_BACKEND}/g" ./terraform/terraform.backend.tf.dist > ./terraform/terraform.backend.tf
+	sed "s/REPLACE_ME/${TERRAFORM_BACKEND}/g" "$(CURDIR)/ci/terraform/terraform.backend.tf.dist" > "$(CURDIR)/ci/terraform/terraform.backend.tf"
 
 .PHONY: terraform/clean
 terraform/clean:
-	rm ./terraform/terraform.backend.tf
+	rm "$(CURDIR)/ci/terraform/terraform.backend.tf"
 
 .PHONY: terraform/provision-ec2
 terraform/provision-ec2: terraform/backend
-	cd ./terraform && \
+	cd "$(CURDIR)/ci/terraform" && \
 	terraform init -reconfigure && \
 	terraform apply -auto-approve
 
 .PHONY: terraform/destroy-ec2
 terraform/destroy-ec2: terraform/backend
-	cd ./terraform && \
+	cd "$(CURDIR)/ci/terraform" && \
     terraform init -reconfigure && \
     terraform destroy -auto-approve
