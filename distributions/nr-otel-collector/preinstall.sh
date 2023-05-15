8d4d7735f06d4c3a9f39f7e2cb1d67875ab679ee
David Sánchez <davidsanchez@newrelic.com>
2023-05-15 18:39:09 +0100

    NR-95230 feat: run as root (systemd default)

--- distributions/nr-otel-collector/preinstall.sh
+++ distributions/nr-otel-collector/preinstall.sh
@@ -13,5 +13,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-getent passwd nr-otel-collector >/dev/null || useradd --system --user-group --no-create-home --shell /sbin/nologin nr-otel-collector
