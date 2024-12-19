784dcf74adaf91c84951bb101eab6c5c24060395
Mailo Arsac <marsac@newrelic.com>
2024-12-19 15:23:45 -0800

    feat: Adding VPC provisioning to EKS module

--- test/terraform/modules/eks_cluster/vars.tf
+++ test/terraform/modules/eks_cluster/vars.tf
@@ -3,16 +3,6 @@ variable "name" {
   type        = string
 }
 
-variable "vpc_id" {
-  description = "VPC id"
-  type        = string
-}
-
-variable "subnet_ids" {
-  description = "subnet ids"
-  type = list(string)
-}
-
 variable "account_id" {
   description = "account id"
   type        = string
--- test/terraform/permanent/main.tf
+++ test/terraform/permanent/main.tf
@@ -2,24 +2,10 @@ data "aws_vpc" "this" {
   id = "vpc-015d2f927c8b5dea7"
 }
 
-data "aws_subnets" "this" {
-  filter {
-    name = "vpc-id"
-    values = [data.aws_vpc.this.id]
-  }
-
-  filter {
-    name = "availability-zone"
-    values = ["us-east-1a", "us-east-1b"]
-  }
-}
 
 module "ci_e2e_cluster" {
   source = "../modules/eks_cluster"
 
   name       = "aws-ci-e2etest"
   account_id = var.aws_account_id
-
-  vpc_id     = data.aws_vpc.this.id
-  subnet_ids = data.aws_subnets.this.ids
 }
