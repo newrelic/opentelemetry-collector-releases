b8f46be7c387fd35124e162fc023f2756ccfd0c5
mailo-nr <marsac@newrelic.com>
2025-01-13 13:28:20 -0800

    Add missing ECR registry url (#182)
    * fix: Skip validation
    
    * fix: Add missing ECR url
    
    * fix: Add nightly tag and update helm release

--- test/terraform/permanent/main.tf
+++ test/terraform/permanent/main.tf
@@ -10,47 +10,6 @@ module "ci_e2e_cluster" {
   account_id = var.aws_account_id
 }
 
-data "aws_eks_cluster_auth" "this" {
-  name = module.ci_e2e_cluster.cluster_name
-}
-
-provider "random" {}
-
-resource "random_string" "hostname_suffix" {
-  length  = 6
-  special = false
-}
-
-resource "helm_release" "ci_e2e_nightly" {
-  depends_on = [module.ci_e2e_cluster]
-
-  name  = "ci-e2etest-nightly"
-  chart = "../../charts/nr_backend"
-
-  create_namespace = true
-  namespace        = "nightly"
-
-  set {
-    name  = "image.pullPolicy"
-    value = "Always"
-  }
-
-  set {
-    name  = "secrets.nrBackendUrl"
-    value = var.nr_backend_url
-  }
-
-  set {
-    name  = "secrets.nrIngestKey"
-    value = var.nr_ingest_key
-  }
-
-  set {
-    name  = "collector.hostname"
-    value = "nr-otel-collector-${var.test_environment}-${random_string.hostname_suffix.result}"
-  }
-}
-
 data "aws_caller_identity" "current" {}
 
 data "aws_iam_session_context" "current" {
@@ -63,6 +22,8 @@ module "ecr" {
   source = "terraform-aws-modules/ecr/aws"
 
   repository_name = "nr-otel-collector"
+
+  repository_image_tag_mutability = "MUTABLE"
 
   repository_read_write_access_arns = [data.aws_iam_session_context.current.issuer_arn]
   repository_read_access_arns = [module.ci_e2e_cluster.cluster_iam_role_arn]
@@ -85,3 +46,55 @@ module "ecr" {
     ]
   })
 }
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.ci_e2e_cluster.cluster_name
+}
+
+provider "random" {}
+
+resource "random_string" "hostname_suffix" {
+  length  = 6
+  special = false
+}
+
+resource "helm_release" "ci_e2e_nightly" {
+  depends_on = [module.ci_e2e_cluster, module.ecr]
+
+  name  = "ci-e2etest-nightly"
+  chart = "../../charts/nr_backend"
+
+  create_namespace = true
+  namespace        = "nightly"
+
+  set {
+    name  = "image.repository"
+    value = module.ecr.repository_url
+  }
+
+  set {
+    name  = "image.tag"
+    value = "nightly"
+  }
+
+  set {
+    name  = "image.pullPolicy"
+    value = "Always"
+  }
+
+  set {
+    name  = "secrets.nrBackendUrl"
+    value = var.nr_backend_url
+  }
+
+  set {
+    name  = "secrets.nrIngestKey"
+    value = var.nr_ingest_key
+  }
+
+  set {
+    name  = "collector.hostname"
+    value = "nr-otel-collector-${var.test_environment}-${random_string.hostname_suffix.result}"
+  }
+}
+
