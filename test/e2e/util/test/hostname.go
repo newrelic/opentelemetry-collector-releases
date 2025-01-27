a85d6f703d56b38a8b2bf63ab9de7c61c9432631
kb-newrelic <121687305+kb-newrelic@users.noreply.github.com>
2025-01-27 11:54:51 -0800

    ci: support distro-dependent tests (#200)

--- test/e2e/util/test/hostname.go
+++ test/e2e/util/test/hostname.go
@@ -3,6 +3,7 @@ package test
 import "fmt"
 
 func NewHostNamePrefix(envName string, deployId string, hostType string) string {
+	// TODO: incorporate distro into hostname when generalizing nightly to support multiple distro
 	// only a prefix as helm chart appends hostId
 	return fmt.Sprintf("%s-%s-%s", envName, deployId, hostType)
 }
@@ -10,6 +11,7 @@ func NewHostNamePrefix(envName string, deployId string, hostType string) string 
 const Wildcard = "%"
 
 func NewNrQueryHostNamePattern(envName string, deployId string, hostType string) string {
+	// TODO: incorporate distro into hostname when generalizing nightly to support multiple distro
 	hostId := Wildcard
 	return fmt.Sprintf("%s-%s-%s-%s", envName, deployId, hostType, hostId)
 }
