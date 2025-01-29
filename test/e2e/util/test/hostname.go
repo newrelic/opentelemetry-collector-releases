78bbca6c920eb1584b0a37179f5f629e62f12a96
kbauer <kbauer@newrelic.com>
2025-01-28 17:12:36 -0800

    ci: revert normalizing host.name in query

--- test/e2e/util/test/hostname.go
+++ test/e2e/util/test/hostname.go
@@ -11,18 +11,13 @@ const (
 )
 
 func NewHostNamePrefix(envName string, deployId string, hostType string) string {
-	distro := getNormalizedDistro()
+	distro := envutil.GetDistro()
 	// only a prefix as helm chart appends hostId
 	return strings.Join([]string{envName, deployId, distro, hostType}, hostNameSegmentSeparator)
 }
 
 func NewNrQueryHostNamePattern(envName string, deployId string, hostType string) string {
-	distro := getNormalizedDistro()
+	distro := envutil.GetDistro()
 	hostId := Wildcard
 	return strings.Join([]string{envName, deployId, distro, hostType, hostId}, hostNameSegmentSeparator)
-}
-
-func getNormalizedDistro() string {
-	// solely to improve readability - no technical necessity
-	return strings.Replace(envutil.GetDistro(), hostNameSegmentSeparator, "_", -1)
 }
